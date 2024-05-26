d82a4bdf3cde3ae16663e940aa4d823298b14c0a
Joel <jsu109@uclive.ac.nz>
2024-05-26 13:42:28 +1200

    scheduler is working now. just need to add tasks

---
+++ scr/scheduler.h
@@ -0,0 +1,55 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define MAX_TASKS 100
+#define KERNAL_PERIOD_US 10000
+typedef struct 
+{
+    uint16_t Id;
+}taskId_t;
+
+typedef struct 
+{
+   taskId_t id;
+   int64_t  period;
+   int64_t taskTick;
+   bool ready;
+   uint8_t priority;
+   void(*run)(void);
+
+}Task_t;
+
+// Define the scheduler struct
+typedef struct
+{  
+    Task_t tasks[MAX_TASKS]; // Array of tasks
+    int8_t numTasks;          // Number of tasks
+    int64_t tickPeriod;       // Tick period for the scheduler
+} scheduler;
+
+  extern scheduler kernal; // Declare the kernal instance
+
+
+
+
+
+
+//set the period for systick interrupts
+void kernal_init(void);
+
+taskId_t register_task(void(*taskEnter)(void), uint8_t priority, int64_t period);
+
+void kernal_start(void);
+
+void kernal_unregister_task(taskId_t taskId);
+
+void kernal_notify_task(taskId_t taskId);
+
+void kernal_block_task(taskId_t taskId);
+
+bool alarm_callback(struct repeating_timer *t);
+
+#endif
---
+++ scr/tick.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "tick.h"
+#include "hardware/structs/systick.h"
+
+sys_timer systick;
+
+void init_systick()
+{ 
+	systick_hw->csr = 0; 	    //Disable 
+	systick_hw->rvr = 124999UL; //Standard System clock (125Mhz)/ (rvr value + 1) = 1ms 
+        systick_hw->csr = 0x7;      //Enable Systic, Enable Exceptions	
+}
+
+extern void isr_systick() //Rewrite of weak systick IRQ in crt0.s file
+{
+	systick.u32_tick_count++;     
+}
+
+uint32_t get_systick()
+{
+	return systick.u32_tick_count;
+}
+
+void init_non_blocking_timer(systick_handler_t *timer , uint32_t u32_time_period_ms,uint8_t u8_user_mode)
+{
+	timer->u32_t0_ms = 0U;
+	timer->u32_timer_period = u32_time_period_ms;
+	timer->timer_enabled = false;
+	timer->u8_mode = u8_user_mode;
+}
+
+void start_non_blocking_timer(systick_handler_t *timer)
+{
+	timer->u32_t0_ms = get_systick();
+	timer->timer_enabled = true;	
+}
+
+void stop_non_blocking_timer(systick_handler_t *timer)
+{
+	timer->timer_enabled = false;
+}
+
+uint8_t non_blocking_timer_expired(systick_handler_t *timer)
+{
+	if(timer->timer_enabled == true)
+	{
+		if ((get_systick() - timer->u32_t0_ms) >= timer->u32_timer_period) //accounts for buffer overflow
+		{
+			if (timer->u8_mode == CONTINOUS_MODE)
+			{
+				start_non_blocking_timer(timer); //Restart Timer for user in Continuous mode
+			}
+			return TIMER_EXPIRED;
+		}
+		else
+		{
+			return TIMER_NOT_EXPIRED;
+		}
+	}
+	return TIMER_NOT_ENABLED;
+}
