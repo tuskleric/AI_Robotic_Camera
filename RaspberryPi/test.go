e4ea715762a5b3947bcf734da4df3918e8e51af2
jordan <jbo101@uclive.ac.nz>
2024-07-08 13:22:24 +1200

    Pi now recieves from dbus and transmits to raspberry pi

--- RaspberryPi/test.go
+++ RaspberryPi/test.go
@@ -17,9 +17,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
-	"time"
 
 	"github.com/godbus/dbus/v5"
 	"periph.io/x/conn/v3/i2c"
@@ -68,6 +66,21 @@ func main() {
 	}
 	defer conn.Close()
 
+	// Initialize periph
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the first available I2C bus
+	bus, err := i2creg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bus.Close()
+
+	// Specify the I2C device address
+	device := &i2c.Dev{Addr: 0x15, Bus: bus}
+
 	// Add match for the specific signal you want to receive
 	if err := conn.AddMatchSignal(
 		dbus.WithMatchObjectPath("/org/cacophony/DBus/test"), //"/org/freedesktop/DBus"),
@@ -85,34 +98,44 @@ func main() {
 		// Print the content of the signal
 		fmt.Printf("Received signal: Path=%s, Sender=%s, Body=%v\n",
 			signal.Path, signal.Sender, signal.Body)
+
+		writeData := []byte{0x01, byte(signal.Body[0].(int32))}
+
+		fmt.Println("Sending bytes:", writeData)
+
+		// Write data to the I2C device
+		_, err = device.Write(writeData)
+		if err != nil {
+			log.Println("Failed to write to device:", err)
+		}
 	}
 }
 
-func main() {
-	// Initialize periph
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
+// func main() {
+// 	// Initialize periph
+// 	if _, err := host.Init(); err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	// Open the first available I2C bus
-	bus, err := i2creg.Open("")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer bus.Close()
+// 	// Open the first available I2C bus
+// 	bus, err := i2creg.Open("")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer bus.Close()
 
-	// Specify the I2C device address
-	device := &i2c.Dev{Addr: 0x15, Bus: bus}
+// 	// Specify the I2C device address
+// 	device := &i2c.Dev{Addr: 0x15, Bus: bus}
 
-	// Send random bytes to the I2C device
-	rand.Seed(time.Now().UnixNano())
-	writeData := []byte{0x01, byte(rand.Intn(256))}
+// 	// Send random bytes to the I2C device
+// 	rand.Seed(time.Now().UnixNano())
+// 	writeData := []byte{0x01, byte(rand.Intn(256))}
 
-	// Write data to the I2C device
-	_, err = device.Write(writeData)
-	if err != nil {
-		log.Fatal("Failed to write to device:", err)
-	}
+// 	// Write data to the I2C device
+// 	_, err = device.Write(writeData)
+// 	if err != nil {
+// 		log.Fatal("Failed to write to device:", err)
+// 	}
 
-	fmt.Println("Sent random bytes:", writeData)
-}
+// 	fmt.Println("Sent byte:", writeData)
+// }
