60656936028106227e0458abda3179aa170f0372
jordan <jbo101@uclive.ac.nz>
2024-08-07 13:20:14 +1200

    Created constants and made a function for converting pixels coords to target angles

--- RaspberryPi/pico_camera_interface/pico_camera_interface_main.go
+++ RaspberryPi/pico_camera_interface/pico_camera_interface_main.go
@@ -11,6 +11,50 @@ import (
 	"periph.io/x/host/v3"
 )
 
+// Constants for D-Bus and I2C configurations
+const (
+	DBUS_PATH          = "/org/cacophony/thermalrecorder"
+	DBUS_NAME          = "org.cacophony.thermalrecorder"
+	I2C_DEVICE_ADDRESS = 0x15
+
+	// Default X
+	AXIS1_PIXEL_WIDTH = 159
+	AXIS1_FOV         = 100
+
+	// Default Y
+	AXIS2_PIXEL_WIDTH = 119
+	AXIS2_FOV         = 50
+
+	INVERT_X    = false
+	INVERT_Y    = false
+	SWITCH_AXES = false
+)
+
+// Function to convert an array of pixel coordinates to degrees
+func convertPixelsToDegree(coords []uint32) (float32, float32) {
+
+	// Find center of target
+	axis1_center := float32(coords[0]+coords[2]) / 2
+	axis2_center := float32(coords[0]+coords[2]) / 2
+
+	// Convert to angle from center
+	axis1_angle := (axis1_center*AXIS1_FOV)/AXIS1_PIXEL_WIDTH - AXIS1_FOV/2
+	axis2_angle := (axis2_center*AXIS2_FOV)/AXIS2_PIXEL_WIDTH - AXIS2_FOV/2
+
+	if INVERT_X {
+		axis1_angle = -axis1_angle
+	}
+	if INVERT_Y {
+		axis2_angle = -axis2_angle
+	}
+
+	if SWITCH_AXES {
+		return axis2_angle, axis1_angle
+	} else {
+		return axis1_angle, axis2_angle
+	}
+}
+
 func main() {
 	conn, err := dbus.ConnectSystemBus()
 	if err != nil {
@@ -32,12 +76,12 @@ func main() {
 	defer bus.Close()
 
 	// Specify the I2C device address
-	device := &i2c.Dev{Addr: 0x15, Bus: bus}
+	device := &i2c.Dev{Addr: I2C_DEVICE_ADDRESS, Bus: bus}
 
 	// Add match for the specific signal you want to receive
 	if err := conn.AddMatchSignal(
-		dbus.WithMatchObjectPath("/org/cacophony/thermalrecorder"), //"/org/freedesktop/DBus"),
-		dbus.WithMatchInterface("org.cacophony.thermalrecorder"),   //"org.freedesktop.DBus"),
+		dbus.WithMatchObjectPath(DBUS_PATH),
+		dbus.WithMatchInterface(DBUS_NAME),
 	); err != nil {
 		panic(err)
 	}
