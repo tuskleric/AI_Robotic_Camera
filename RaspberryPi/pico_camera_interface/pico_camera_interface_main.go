1fef581636bbc8db0892ce45ebd532e21f8d7220
jordan <jbo101@uclive.ac.nz>
2024-08-07 13:38:10 +1200

    Integrated some data processing and improved modularity

--- RaspberryPi/pico_camera_interface/pico_camera_interface_main.go
+++ RaspberryPi/pico_camera_interface/pico_camera_interface_main.go
@@ -30,6 +30,49 @@ const (
 	SWITCH_AXES = false
 )
 
+func initI2C() *i2c.Dev {
+	// Initialize periph
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open the first available I2C bus
+	bus, err := i2creg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bus.Close()
+
+	// Specify the I2C device address
+	device := &i2c.Dev{Addr: I2C_DEVICE_ADDRESS, Bus: bus}
+
+	return device
+}
+
+func initDBUS() (*dbus.Conn, chan *dbus.Signal) {
+	// Connect to the system bus
+	conn, err := dbus.ConnectSystemBus()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	// Add match for the specific signal you want to receive
+	if err := conn.AddMatchSignal(
+		dbus.WithMatchObjectPath(DBUS_PATH),
+		dbus.WithMatchInterface(DBUS_NAME),
+	); err != nil {
+		panic(err)
+	}
+
+	// Channel to receive signals
+	c := make(chan *dbus.Signal, 10)
+	conn.Signal(c)
+
+	return conn, c
+}
+
 // Function to convert an array of pixel coordinates to degrees
 func convertPixelsToDegree(coords []uint32) (float32, float32) {
 
@@ -56,52 +99,32 @@ func convertPixelsToDegree(coords []uint32) (float32, float32) {
 }
 
 func main() {
-	conn, err := dbus.ConnectSystemBus()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
-		os.Exit(1)
-	}
-	defer conn.Close()
 
-	// Initialize periph
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
+	device := initI2C()
 
-	// Open the first available I2C bus
-	bus, err := i2creg.Open("")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer bus.Close()
-
-	// Specify the I2C device address
-	device := &i2c.Dev{Addr: I2C_DEVICE_ADDRESS, Bus: bus}
-
-	// Add match for the specific signal you want to receive
-	if err := conn.AddMatchSignal(
-		dbus.WithMatchObjectPath(DBUS_PATH),
-		dbus.WithMatchInterface(DBUS_NAME),
-	); err != nil {
-		panic(err)
-	}
-
-	// Channel to receive signals
-	c := make(chan *dbus.Signal, 10)
-	conn.Signal(c)
+	conn, c := initDBUS()
 
 	// Listening for signals
 	for signal := range c {
+		// Extract data
+		what := signal.Body[0].(string)
+		confidence := signal.Body[1].(int32)
+		coords := signal.Body[2].([]uint32)
+		tracking := signal.Body[3].(string)
+
+		// Process data
+		x_angle, y_angle := convertPixelsToDegree(coords)
+
 		// Print the content of the signal
-		fmt.Printf("Received signal: Path=%s, Sender=%s, Body=%v\n",
-			signal.Path, signal.Sender, signal.Body)
+		fmt.Printf("What = %s, Confidence = %d, Location = X: %f Y: %f, Tracking = %s\n",
+			what, confidence, x_angle, y_angle, tracking)
 
 		writeData := []byte{0x01, byte(signal.Body[0].(int32))}
 
 		fmt.Println("Sending bytes:", writeData)
 
 		// Write data to the I2C device
-		_, err = device.Write(writeData)
+		_, err := device.Write(writeData)
 		if err != nil {
 			log.Println("Failed to write to device:", err)
 		}
